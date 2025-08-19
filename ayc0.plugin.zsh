ee50813b496dd2840055d0d8d630f8d033b83d04
Benjamin <ayc0.benj@gmail.com>
2025-08-19 15:17:25 +0200

    Use `git config --remove-section "branch.<>"` instead of `git config --unset "branch.<>.…"`

--- ayc0.plugin.zsh
+++ ayc0.plugin.zsh
@@ -48,11 +48,7 @@ dd-done() {
     git branch -D $targetBranch
   fi
 
-  git config --unset "branch.$targetBranch.remote"
-  git config --unset "branch.$targetBranch.merge"
-  git config --unset "branch.$targetBranch.vscode-merge-base"
-  git config --unset "branch.$targetBranch.github-pr-owner-number"
-  git config --unset "branch.$targetBranch.github-pr-base-branch"
+  git config --remove-section "branch.$targetBranch"
 
   return 0
 }
@@ -61,23 +57,17 @@ dd-clean-branches() {
   for branch in $(git branch --merged $mainBranch | grep -vE '^[ *]*(prod|preprod)$');
   do
     git branch -d $branch
-    git config --unset "branch.$branch.remote"
-    git config --unset "branch.$branch.merge"
-    git config --unset "branch.$branch.vscode-merge-base"
+    git config --remove-section "branch.$branch"
   done
   for branch in $(git branch | grep '\--fix-staging');
   do
     git branch -D $branch
-    git config --unset "branch.$branch.remote"
-    git config --unset "branch.$branch.merge"
-    git config --unset "branch.$branch.vscode-merge-base"
+    git config --remove-section "branch.$branch"
   done
   for branch in $(git branch | grep -E 'staging-[0-9]+');
   do
     git branch -D $branch
-    git config --unset "branch.$branch.remote"
-    git config --unset "branch.$branch.merge"
-    git config --unset "branch.$branch.vscode-merge-base"
+    git config --remove-section "branch.$branch"
   done
   return 0
 }
